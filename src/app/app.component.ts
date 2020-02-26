3e90bfbfe1579feff7e035621679c7aa82b80d32
Nhan Vu <nhan997@gmail.com>
2020-02-26 22:39:08 +0100

    add ngx bootstrap and bootswatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'DatingApp-SPA';
   jwtHelper = new JwtHelperService();
 
