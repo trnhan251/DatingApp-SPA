17322354e7838ea70c96368df560e91745e925b7
Nhan Vu <nhan997@gmail.com>
2020-03-02 10:59:28 +0100

    fix jwtmodule config 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://datingservice.azurewebsites.net/api/',
-  url: 'https://datingservice.azurewebsites.net'
+  apiUrl: 'datingservice.azurewebsites.net/api/',
+  url: 'datingservice.azurewebsites.net'
 };
